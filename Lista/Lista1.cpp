b6bc75c43c6e4199807238bb05a251c15fa7c6cf
Calebe Ribeiro <rcalebe655@gmail.com>
2022-08-24 09:28:06 -0300

    introduction list

---
+++ Lista/Lista1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#define MaxTam 4
+
+using namespace std;
+
+void criarLista(TipoLista &lista);
+int verificaListaVazia(TipoLista &lista);
+int insereLista(TipoLista &lista, TipoItem item);
+void imprimeLista(TipoLista &lista);
+
+typedef struct TipoItem
+{
+    int chave;
+    char letra;
+};
+
+typedef struct TipoLista
+{
+    TipoItem item[MaxTam];
+    int tamanho = 0;
+};
+
+int main(){
+    TipoLista lista;
+    TipoItem item;
+    item.chave = 7;
+    item.letra = 'c';
+    criarLista(lista);
+    insereLista(lista, item);
+    imprimeLista(lista);
+    return 0;
+}
+
+void criarLista(TipoLista &lista){
+    lista.tamanho = 0;
+}
+
+int verificaListaVazia(TipoLista &lista){
+    if(lista.tamanho == 0)
+        return 0;
+    return 1;
+}
+
+int insereLista(TipoLista &lista, TipoItem item){
+    if(lista.tamanho < MaxTam){
+        lista.item[lista.tamanho].chave = item.chave;
+        lista.item[lista.tamanho].letra = item.letra;
+        lista.tamanho++;
+        return 1;
+    }
+    return 0;
+}
+
+void imprimeLista(TipoLista &lista){
+    for(int i = 0; i < lista.tamanho; i++){
+        cout<< lista.item[i].chave<<endl;
+        cout<< lista.item[i].letra<<endl;
+    }
+}
