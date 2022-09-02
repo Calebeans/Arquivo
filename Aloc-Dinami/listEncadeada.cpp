b0f787e76f639b4a7cff5fb66ab182cbdc8437d1
Calebe Ribeiro <rcalebe655@gmail.com>
2022-09-02 09:17:56 -0300

    Linked list

--- Aloc-Dinami/listEncadeada.cpp
+++ Aloc-Dinami/listEncadeada.cpp
@@ -1,63 +1,103 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <windows.h>
+#include "lista.hpp"
+
+#define MAXTAM 5
 
 using namespace std;
 
-void criarListaVazia (TipoLista *lista);
-bool verificaListaVazia (TipoLista *lista);
-void inserePrimeiroElemento(TipoLista *lista, TipoItem *item);
-
-typedef struct TipoItem
+void CriaListaVazia(TipoLista *lista)
 {
-    int id;
-    char nome[82];
-} TipoItem;
+    if (!listaCriada)
+    {
+        lista->tamanho = 0;
+        lista->Primeiro = 0;
+        lista->Ultimo = lista->Primeiro;
+        cout << "Lista criada com sucesso!";
+        listaCriada = true;
+    }
+    else
+    {
+        cout << "Lista já existe!";
+    }
+    Sleep(1500);
+}
 
-typedef struct TipoElemento* Apontador;
-
-typedef struct TipoElemento
+int VerificaListaVazia(TipoLista *lista)
 {
-    TipoItem item;
-    struct TipoElemento* prox;
-} TipoElemento;
+    return (lista->Primeiro == lista->Ultimo);
+}
 
-typedef struct TipoLista
+int VerificaListaCheia(TipoLista *lista)
 {
-    Apontador primeiro;
-    Apontador ultimo;
-} TipoLista;
-
-int main(){
-
-
-    return 0;
+    return (lista->Ultimo == MAXTAM);
 }
 
-void criarListaVazia (TipoLista *lista){
-    lista->primeiro = new TipoElemento;
-    lista->ultimo = lista->primeiro;
-    lista->ultimo->prox = NULL; 
+int TamanhoLista(TipoLista *lista)
+{
+    return lista->tamanho;
 }
 
-bool verificaListaVazia (TipoLista *lista){
-    return lista->primeiro == lista->ultimo;
+int InsereItem(TipoLista *lista, TipoItem item)
+{
+    if (VerificaListaCheia(lista))
+    {
+        return -1;
+    }
+    lista->Item[lista->Ultimo].chave = item.chave;
+    lista->tamanho++;
+    lista->Ultimo++;
+    return 1; 
 }
 
-void inserePrimeiroElemento(TipoLista *lista, TipoItem *item){
-    Apontador  aux;
-    aux = lista->primeiro->prox;
-    lista->primeiro->prox = new TipoElemento;
-    lista->primeiro->prox->prox = aux;
-    lista->primeiro->prox->item = *item;
-    lista->ultimo = lista->primeiro->prox;
+void ImprimeLista(TipoLista lista)
+{
+    if (listaCriada)
+    {
+        int Aux;
+        for (Aux = lista.Primeiro; Aux <= (lista.Ultimo - 1); Aux++)
+        {
+            cout << "Chave: " << lista.Item[Aux].chave << endl;
+        }
+        cout << endl;
+        system("Pause");
+    }
+    else
+    {
+        cout << "Crie a lista primeiramente.";
+        Sleep(1000);
+    }
 }
 
-void pecorreLista (TipoLista *lista, TipoItem *item){
-    
+int PesquisaItem(TipoLista *lista, int chave)
+{
+    for (int i = lista->Primeiro; i < lista->Ultimo; i++)
+    {
+        if (chave == lista->Item[i].chave)
+        {
+            return i; // Retorna posição do item encontrado
+        }
+    }
+    return -1;
 }
 
-void insereListaAposElemento(TipoLista *lista, TipoItem item, Apontador x){
-    Apontador aux;
-    aux = lista.
+void RetiraItem(TipoApontador p, TipoLista *lista, TipoItem *item)
+{
+    int Aux;
+
+    if (VerificaListaVazia(lista) || p > lista->Ultimo)
+    {
+        cout << "Erro: Posição não existe na lista\n";
+        return;
+    }
+
+    *item = lista->Item[p];
+
+    for (Aux = p; Aux <= lista->Ultimo; Aux++)
+    {
+        lista->Item[Aux] = lista->Item[Aux + 1];
+    }
+
+    lista->Ultimo--;
+    lista->tamanho--;
 }
